67ae7d408a6b85ffded2674cf04e3ba4bd3aec94
Rodrigo Oliveira <rodrigocode4@gmail.com>
2018-08-29 10:16:25 -0300

    Add metodos @@iterator: entrues, keys, values; além de outros do Array.from e Array.of

--- 01-Array/metodos.js
+++ 01-Array/metodos.js
@@ -8,7 +8,7 @@ console.log('1.', numbers);
 //--------------------every()----------------------// itera cada elemento até que a função retorne false
 let isEven = x => {
     console.log(x);
-	return !!(x % 2 == 0);
+    return !!(x % 2 == 0);
 }
 let arrNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 console.log('2.', arrNumbers.every(isEven))
@@ -40,7 +40,53 @@ for (let n of arrNumbers) {
 //--------------------@@iterator - [Symbol.iterator]()----------------------// chama cada elemento do array por métodos, para saber o seu valor. Ex: next().value
 let iterator = arrNumbers[Symbol.iterator]();
 
-console.log(iterator.next().value);
-console.log(iterator.next().value);
-console.log(iterator.next().value);
-console.log(iterator.next().value);
+console.log('8.', iterator.next().value);
+console.log('8.', iterator.next().value);
+console.log('8.', iterator.next().value);
+console.log('8.', iterator.next().value);
+
+//--------------------@@iterator - entrues()----------------------// método para obtenção de chave e valor de um array: [key, value]. Em caso de somente números no array teremos [posição, valor];
+let aEntrues = arrNumbers.entries();
+
+console.log('9.', aEntrues.next().value);
+console.log('9.', aEntrues.next().value);
+console.log('9.', aEntrues.next().value);
+console.log('9.', aEntrues.next());
+
+//--------------------@@iterator - keys()----------------------// método para obtenção da chave de um array. Em caso de somente números no array teremos, a chave será a posição deste;
+
+let aKey = arrNumbers.keys();
+
+console.log('10.', aKey.next());
+console.log('10.', aKey.next());
+console.log('10.', aKey.next().value);
+console.log('10.', aKey.next().value);
+
+//--------------------@@iterator - value()----------------------// método para obtenção do valor de um array.
+
+let aValues = arrNumbers.values();
+
+console.log('11.', aValues.next().value);
+console.log('11.', aValues.next().value);
+console.log('11.', aValues.next().value);
+console.log('11.', aValues.next().value);
+
+//--------------------Array.from()----------------------// método para criação de um novo array a partir de um existente, podendo estabelecer condições para formação do novo array. Exemplo: arrNumbers2_1, que retona true ou false;
+let arrNumbers2 = Array.from(arrNumbers);
+
+console.log('12.', arrNumbers2);
+
+
+let arrNumbers2_1 = Array.from(arrNumbers, x => (x % 2 == 0));
+
+console.log('12.1.', arrNumbers2_1);
+
+//--------------------Array.of()----------------------// método que cria um array a partir dos argumentos passados, que são os elementos. É uma notação diferente, mas como a mesma aplicabilidade do array literal: array = [..];
+let arrNumbers3 = Array.of(1, 2, 3, 4, 5, 6, 7, 8);
+
+console.log(arrNumbers3)
+
+// neste exemplo usamos o operador spread (...), essa notaçã de três pontinhos. Esse operador poder ser usao para receber parametros sem um limite de tamanho.
+let arrNumbers3Copy = Array.of(-1, 0, ...arrNumbers3)
+
+console.log(arrNumbers3Copy);
