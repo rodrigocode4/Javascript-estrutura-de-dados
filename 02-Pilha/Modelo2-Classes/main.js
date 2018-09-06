446ab2e4bfbfc00f15879e35c2a8fabd840f7f5d
Rodrigo Oliveira <rodrigocode4@gmail.com>
2018-09-06 20:14:43 -0300

    Implementação de pilha com Classe (ES6)

---
+++ 02-Pilha/Modelo2-Classes/main.js
@@ -0,0 +1,28 @@
+const Stack = require('./stack');
+
+let stack = new Stack();
+
+console.log(stack.isEmpty());
+
+stack.push(4);
+stack.push(8);
+stack.push(20);
+stack.push(33);
+
+console.log(stack.isEmpty());
+
+console.log(stack.peek());
+
+console.log(stack.size());
+
+stack.pop();
+
+console.log(stack.peek());
+
+stack.print();
+
+stack.clear();
+
+stack.print(); 
+
+console.log(stack.isEmpty());
---
+++ 02-Pilha/Modelo2-Classes/stack.js
@@ -0,0 +1,36 @@
+class Stack {
+    constructor() {
+        this.items = [];
+    }
+
+    push(element) {
+        this.items.push(element);
+    }
+
+    pop() {
+        return this.items.pop();
+    }
+
+    isEmpty() {
+        return this.items.length == 0;
+    }
+
+    peek() {
+        return this.items[this.items.length - 1];
+    }
+
+    size() {
+        return this.items.length;
+    }
+
+    clear() {
+        this.items = [];
+    }
+
+    print() {
+        console.log(this.items.join(' | '));
+    }
+
+}
+
+module.exports = Stack;
